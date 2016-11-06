bccbb51a0769ded6eb6d7566a73b274d214a7028
Zahid Saleem Uddin Jeelani <zjeelani@zahids-macbook-pro.local>
2016-11-05 23:18:18 -0700

    GitHub changes comment

--- Existing Project/ViewController.swift
+++ Existing Project/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // GitHub Changes
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
